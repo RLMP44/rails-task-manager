497eceb16bbaae93d3e014e96d32d60e338704eb
Rachael Momber <rlmomber@gmail.com>
2023-11-07 14:51:01 +0900

    finish rails-task-manager with optional refactor, no optional bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
-  # new must be before show
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task
 
+  # # Display all tasks
+  # get 'tasks', to: 'tasks#index'
+  # # Create a new task
+  # # New must be before Show
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # Show all tasks
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # After refactor
+  resources :tasks
 end
